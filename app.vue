5cce757f81a2a59e30806879a56a008816bebbf5
TARIK ÖZ <125669805+tarik-oz@users.noreply.github.com>
2025-06-09 18:00:59 +0300

    feat: add landing page (#50)

--- app.vue
+++ app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl underline">
-      Bababaaa
-    </h1>
-    <button class="btn btn-primary">
-      Primary
-      <Icon name="tabler:home" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-accent">
+        Sign In With GitHub
+        <Icon name="tabler:brand-github" size="1.5rem" />
+      </a>
+    </div>
+  </div>
+</template>
