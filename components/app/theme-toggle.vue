3d23352731029b24d149084b5bee0bd3cafa2dc5
TARIK ÖZ <125669805+tarik-oz@users.noreply.github.com>
2025-06-10 07:50:05 +0300

    feat: add color mode support , add theme toggle button to navbar (#51)

---
+++ components/app/theme-toggle.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value) {
+    colorMode.preference = value ? "dark" : "light";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate mx-4">
+    <input v-model="isDark" type="checkbox">
+    <Icon
+      name="tabler:sun"
+      class="swap-on"
+      size="1.5rem"
+    />
+    <Icon
+      name="tabler:moon-filled"
+      class="swap-off"
+      size="1.5rem"
+    />
+  </label>
+</template>
