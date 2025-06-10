1766170327da9302e1b8dad89c536b21f0a1d7e5
tarik-oz <tarikoz201007@gmail.com>
2025-06-10 15:58:09 +0300

    chore: integrate better-auth package and update environment variables for authentication

--- lib/env.ts
+++ lib/env.ts
@@ -4,6 +4,8 @@ import tryParseEnv from "./try-parse-env";
 
 const EnvSchema = z.object({
   DB_FILE_NAME: z.string(),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
