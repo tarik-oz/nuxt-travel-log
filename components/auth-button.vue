10c519f96d3b854b04d4c11b6a699d3a8cf7ed0d
tarik-oz <tarikoz201007@gmail.com>
2025-06-10 17:12:05 +0300

    chore: add Pinia for state management and update authentication to use GitHub social login

---
+++ components/auth-button.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <button
+    :disabled="authStore.loading"
+    class="btn btn-accent"
+    @click="authStore.signIn"
+  >
+    Sign In With GitHub
+    <span v-if="authStore.loading" class="loading loading-dots loading-md" />
+    <Icon
+      v-else
+      name="tabler:brand-github"
+      size="1.5rem"
+    />
+  </button>
+</template>
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import db from "./db/index";
+import env from "./env";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -10,6 +11,12 @@ export const auth = betterAuth({
   advanced: {
     database: {
       generateId: false,
+    },
+  },
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     },
   },
 });
