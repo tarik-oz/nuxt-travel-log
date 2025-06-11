352df3caeab8da2cd412905c5368a57e287b1091
TARIK ÖZ <125669805+tarik-oz@users.noreply.github.com>
2025-06-11 18:49:16 +0300

    feat: add auth middleware, ssr auth session (#57)

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -9,7 +13,14 @@
           Keep track of your travels and adventures with this simple travel log app.
           Add locations, photos, and notes to create a digital journal of your journeys.
         </p>
-        <AuthButton />
+        <AuthButton v-if="!authStore.user" />
+        <NuxtLink
+          v-else
+          to="/dashboard"
+          class="btn btn-primary"
+        >
+          Start Logging
+        </NuxtLink>
       </div>
     </div>
   </div>
