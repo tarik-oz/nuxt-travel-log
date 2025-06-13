d92316055fb8b39934fc0f3f9fd5e50995a174d7
tarik-oz <tarikoz201007@gmail.com>
2025-06-13 20:54:08 +0300

    feat: integrate nuxt-csurf for CSRF protection and enhance location form with validation

---
+++ components/app/form-field.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{
+  name: string;
+  label: string;
+  type?: "text" | "textarea" | "number";
+  error?: string;
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <fieldset class="fieldset">
+    <legend class="fieldset-legend">
+      {{ props.label }}
+    </legend>
+    <Field
+      :as="type === 'textarea' ? 'textarea' : 'input'"
+      :name="props.name"
+      :type="type || 'text'"
+      :disabled="props.disabled"
+      class="w-full"
+      :class="{
+        'input-error': props.error,
+        'input': !type || type === 'text' || type === 'number',
+        'textarea': type === 'textarea',
+      }"
+    />
+    <p v-if="props.error" class="fieldset-label text-error">
+      {{ props.error }}
+    </p>
+  </fieldset>
+</template>
