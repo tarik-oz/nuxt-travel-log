ac4dc6ca722b820d12d5378a74771a5cb5a943bc
tarik-oz <tarikoz201007@gmail.com>
2025-06-13 20:55:03 +0300

    fix: eslint nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@pinia/nuxt", "@vee-validate/nuxt"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@pinia/nuxt", "@vee-validate/nuxt", "nuxt-csurf"],
   eslint: {
     config: {
       standalone: false,
