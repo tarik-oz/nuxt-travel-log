454377e62e5a850deac946750aa6607b34fb1377
tarik-oz <tarikoz201007@gmail.com>
2025-06-15 11:11:05 +0300

    feat: add locations to the sidebar

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
-const { data, status } = await useFetch("/api/locations", {
-  lazy: true,
+import { useLocationStore } from "~/stores/location";
+
+const locationsStore = useLocationStore();
+const { locations, status } = storeToRefs(locationsStore);
+onMounted(() => {
+  locationsStore.refresh();
 });
 </script>
 
@@ -12,9 +16,9 @@ const { data, status } = await useFetch("/api/locations", {
     <div v-if="status === 'pending'">
       <span class="loading loading-spinner loading-xl" />
     </div>
-    <div v-else-if="data && data.length > 0" class="flex flex-wrap mt-4 gap-2">
+    <div v-else-if="locations && locations.length > 0" class="flex flex-wrap mt-4 gap-2">
       <div
-        v-for="location in data"
+        v-for="location in locations"
         :key="location.id"
         class="card card-compact bg-base-300 h-40 w-72"
       >
