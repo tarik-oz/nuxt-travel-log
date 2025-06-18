fb6961a8b1c4358d343686859632357942fd24c3
TARIK ÖZ <125669805+tarik-oz@users.noreply.github.com>
2025-06-18 20:59:15 +0000

    feat: enhance map and sidebar interaction with location details and styling updates (#67)

--- stores/map.ts
+++ stores/map.ts
@@ -2,6 +2,13 @@ import type { MapPoint } from "~/lib/types";
 
 export const useMapStore = defineStore("useMapStore", () => {
   const mapPoints = ref<MapPoint[]>([]);
+  const selectedPoint = ref<MapPoint | null>(null);
+  const shouldFlyTo = ref(true);
+
+  function selectPointWithoutFlyTo(point: MapPoint | null) {
+    shouldFlyTo.value = false;
+    selectedPoint.value = point;
+  }
 
   async function init() {
     const { useMap } = await import("@indoorequal/vue-maplibre-gl");
@@ -9,12 +16,14 @@ export const useMapStore = defineStore("useMapStore", () => {
 
     const map = useMap();
 
+    let bounds: LngLatBounds | null = null;
+
     effect(() => {
       const firstPoint = mapPoints.value[0];
       if (!firstPoint)
         return;
 
-      const bounds = mapPoints.value.reduce((bounds, point) => {
+      bounds = mapPoints.value.reduce((bounds, point) => {
         return bounds.extend([point.long, point.lat]);
       }, new LngLatBounds(
         [firstPoint.long, firstPoint.lat],
@@ -25,10 +34,29 @@ export const useMapStore = defineStore("useMapStore", () => {
         padding: 60,
       });
     });
+
+    effect(() => {
+      if (selectedPoint.value) {
+        if (shouldFlyTo.value) {
+          map.map?.flyTo({
+            center: [selectedPoint.value.long, selectedPoint.value.lat],
+            speed: 0.8,
+          });
+        }
+        shouldFlyTo.value = true;
+      }
+      else if (bounds) {
+        map.map?.fitBounds(bounds, {
+          padding: 60,
+        });
+      }
+    });
   }
 
   return {
     init,
     mapPoints,
+    selectedPoint,
+    selectPointWithoutFlyTo,
   };
 });
