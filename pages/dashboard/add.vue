c2ae5fed0f88a70f4849cc22ecb0b1b886cbc95d
TARIK ÖZ <125669805+tarik-oz@users.noreply.github.com>
2025-06-22 20:42:39 +0000

    feat: add location search (#69)

--- pages/dashboard/add.vue
+++ pages/dashboard/add.vue
@@ -33,6 +33,7 @@ const onSubmit = handleSubmit(async (values) => {
       body: values,
     });
     submitted.value = true;
+    mapStore.addedPoint = null;
     navigateTo("/dashboard");
   }
   catch (e) {
@@ -40,7 +41,7 @@ const onSubmit = handleSubmit(async (values) => {
     if (error.data?.data) {
       setErrors(error.data?.data);
     }
-    submitError.value = error.data?.statusMessage || error.statusMessage || "An unknown error occurred";
+    submitError.value = getFetchErrorMessage(error);
   }
   loading.value = false;
 });
@@ -49,6 +50,18 @@ function formatNumber(value?: number) {
   if (!value)
     return 0;
   return value.toFixed(5);
+}
+
+function searchResultSelected(result: NominatimResult) {
+  setFieldValue("name", result.display_name);
+  mapStore.addedPoint = {
+    id: 1,
+    name: "Added Point",
+    description: "",
+    long: Number(result.lon),
+    lat: Number(result.lat),
+    centerMap: true,
+  };
 }
 
 effect(() => {
@@ -112,15 +125,23 @@ onBeforeRouteLeave(() => {
         :error="errors.description"
         :disabled="loading"
       />
-      <p>
-        Drag the <Icon name="tabler:map-pin-filled" class="text-warning" /> marker to your desired location.
-      </p>
-      <p>
-        Or double click on the map to set the location.
-      </p>
       <p class="text-xs text-gray-400">
-        Current location: {{ formatNumber(controlledValues.lat) }}, {{ formatNumber(controlledValues.long) }}
+        Current coordinates: {{ formatNumber(controlledValues.lat) }}, {{ formatNumber(controlledValues.long) }}
       </p>
+      <p>
+        To set the coordinates:
+      </p>
+      <ul class="list-disc ml-4 text-sm">
+        <li>
+          Drag the <Icon name="tabler:map-pin-filled" class="text-warning" /> marker on the map.
+        </li>
+        <li>
+          Double click the map.
+        </li>
+        <li>
+          Search for a location below.
+        </li>
+      </ul>
       <div class="flex justify-end gap-2">
         <button
           :disabled="loading"
@@ -149,5 +170,7 @@ onBeforeRouteLeave(() => {
         </button>
       </div>
     </form>
+    <div class="divider" />
+    <AppPlaceSearch @result-selected="searchResultSelected" />
   </div>
 </template>
